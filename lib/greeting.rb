c8f641d6f74b7c46593e9369dd42840fa710e764
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-18 19:21:25 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,8 @@
 # code the #greeting method here!
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+  puts "Hi! I'm HAL, what's your name?"
+  name = get.strip
+end
+
+greeting(name)
